4a0fd39a59af69135c6cec4294d661bf33b95ce7
Apprentice <apprentice@dbc17.local>
2013-10-29 18:36:28 -0700

    deleted useless helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  private
-
-  def current_user
-  	@current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
-  end
 end
